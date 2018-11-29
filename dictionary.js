f2c2d0a14a6a3382c286acf6fa6580bae6bdf89d
Apoorv Vardhan <vardhanapoorv@gmail.com>
2018-11-29 11:03:18 +0530

    Serverless ipsum generator added

---
+++ dictionary.js
@@ -0,0 +1,14 @@
+module.exports = [
+    'auto-scaling',
+    'zero-maintenance',
+    'pay-per-execution',
+    'serverdeath',
+    'function',
+    'event',
+    'handler',
+    'cloud',
+    'NoOps',
+    'Lambda',
+    'microservices',
+    'monitoring',
+    ]
---
+++ handler.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const ipsum = require("lorem-ipsum")
+const dictionary = require("./dictionary")
+
+module.exports.ipsum = (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    body: ipsum({
+      words: dictionary,
+    }),
+  };
+callback(null, response);
+};
