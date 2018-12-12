073ae759b7c62209f5f0d4b84934509a418ac196
devbatmanmclaren <45826140+devbatmanmclaren@users.noreply.github.com>
2018-12-12 22:21:26 +0530

    Update handler.js

--- handler.js
+++ handler.js
@@ -2,7 +2,7 @@
 
 const ipsum = require("lorem-ipsum")
 const dictionary = require("./dictionary")
-
+// hheggg
 module.exports.ipsum = (event, context, callback) => {
   const response = {
     statusCode: 200,
